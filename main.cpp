00799bda8cf0a760c639d09483810bcc54254044
giroOrig <lentesyflores@gmail.com>
2020-04-08 19:22:34 -0300

    modificacionesv2

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Contacto 
+{
+    private:
+        string email;
+        string nombre;
+        int telefono;
+    
+    public:
+        Contacto(){}
+
+        ~Contacto(){}
+
+        void setContacto(string em, string nom, int tel)  
+        {
+            nombre = nom;
+            telefono = tel;
+            email = em;
+        }
+        string getEmail()
+        {
+            return email;
+        }
+
+        string getNombre() 
+        {
+            return nombre;
+        }
+        int getTelefono() 
+        {
+            return telefono;
+        }
+};
+
+class Agenda 
+{
+    private:    
+        list <Contacto> contactos;
+    
+    public:
+        ~Agenda() {}
+
+        void addContacto(Contacto* nContacto)
+        {
+            contactos.push_back(*nContacto);
+        }
+
+        void listarContactos()
+        {
+            cout<<"-------------------------"<<endl;
+            cout<<"   Agenda de Contactos    "<<endl;
+            cout<<"-------------------------"<<endl;
+
+            list<Contacto>::iterator ite;
+                for(ite=contactos.begin(); ite != contactos.end(); ite++)
+                {
+                    cout << "Nombre:  " << ite->getNombre() << endl;
+                    cout << "Telefono:  " << ite->getTelefono() << endl;
+                    cout << "Email: " << ite->getEmail() << endl;
+                }
+
+            cout<<"-------------------------"<<endl;
+        }
+};
+
+int main() 
+{
+        Contacto* contacto = new Contacto();
+        contacto->setContacto("cel@g.com", "Celes", 4324324);
+        //cout<<contacto->getNombre()<<endl;
+
+        Contacto* contacto2 = new Contacto();
+        contacto2->setContacto("lu@g.com", "Luis", 9399929);
+
+        Agenda* agenda = new Agenda();
+
+        agenda->addContacto(contacto);
+        agenda->addContacto(contacto2);
+
+        agenda->listarContactos();
+
+        delete contacto;
+        delete agenda;
+        return 0;
+}
